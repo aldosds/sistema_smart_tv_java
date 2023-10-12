68c275c609146eed703f5379266982ee59a3a2ce
Aldo Santos <aldoacesso@gmail.com>
2023-10-11 23:55:22 -0300

    Criado condição ternária

---
+++ controle-fluxo-condicional/src/ResultadoEscolarTernario.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolarTernario {
+    
+    public static void main(String[] args) {
+        
+        int nota = 5;
+
+        String resultado = nota >= 7 ? "Aprovado!" : "Reporvado!";
+
+        System.out.println(resultado);
+        
+    }
+}
