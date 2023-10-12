6bf522bb3a7aa7ead3405a307e7fe6421923fd9e
Aldo Santos <aldoacesso@gmail.com>
2023-10-12 00:37:36 -0300

    Criado uma condição Switch Case sem Break

---
+++ controle-fluxo-condicional/src/PlanoOperadoraSwitchSemBreak.java
@@ -0,0 +1,19 @@
+public class PlanoOperadoraSwitchSemBreak {
+    
+    public static void main(String[] args) {
+        
+        String plano = "M";
+
+        switch(plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e Instagram Grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
